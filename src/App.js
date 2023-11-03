0a81c5453bb0c1b34fc0245a0555dc7b4fb44700
CHOHUNE <yuureru@gmail.com>
2023-11-03 11:50:35 +0900

    변수의 값이 변경 되어도 바로 dom에 반영되지 않음 (react)

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import { Button, Input } from "@chakra-ui/react";
 
 for (let i = 0; i < 5; i++) {}
 function App(props) {
+  let text = "initial text";
   function handleChange(e) {
-    console.log("인풋 값이 변경됨");
     console.log(e.target.value);
+    text = e.target.value;
+    console.log("text", text);
   }
-
   //  이벤트를 받고 싶으면 핸들체인지 함수에 e로 파라메터를 받음(event)
   //  children 과 비슷하게 target은 받는 함수를 말함
   return (
     <div>
+      <p>{text}</p>
       <Input onChange={handleChange}></Input>
       <Input onChange={handleChange}></Input>
       {/*  서로 다른 엘레멘트도 서로 다른 밸류값이 출력됨 */}
