5dcb52039960883a2538aaeba8f8350a6661ad13
CHOHUNE <yuureru@gmail.com>
2023-11-03 12:25:13 +0900

    EVENT PREVENT

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React from "react";
 import { Button, Input } from "@chakra-ui/react";
 
-for (let i = 0; i < 5; i++) {}
 function App(props) {
-  let text = "initial text";
-  function handleChange(e) {
-    console.log(e.target.value);
-    text = e.target.value;
-    console.log("text", text);
+  function handleClick(e) {
+    e.preventDefault();
+    console.log("다른 일을 시킴");
   }
-  //  이벤트를 받고 싶으면 핸들체인지 함수에 e로 파라메터를 받음(event)
-  //  children 과 비슷하게 target은 받는 함수를 말함
+  function handleSubmit(e) {
+    e.preventDefault();
+    //   기본기능취소
+    console.log("다른일시킴 ");
+  }
   return (
     <div>
-      <p>{text}</p>
-      <Input onChange={handleChange}></Input>
-      <Input onChange={handleChange}></Input>
-      {/*  서로 다른 엘레멘트도 서로 다른 밸류값이 출력됨 */}
+      <a href="https://www.naver.com" onClick={handleClick}>
+        네 이 년
+      </a>
+      <div>
+        <form action="https://www.daum.net/search" onSubmit={handleSubmit}>
+          <input type="text" name="q" />
+          <button>검색</button>
+        </form>
+      </div>
     </div>
   );
 }
