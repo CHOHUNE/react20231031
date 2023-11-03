1d1ae7be762be64e3dfe0e3b4fba23f8eb932beb
CHOHUNE <yuureru@gmail.com>
2023-11-03 11:40:09 +0900

    event target 연습 1

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import React from "react";
 import { Button } from "@chakra-ui/react";
 
+for (let i = 0; i < 5; i++) {}
 function App(props) {
-  // 함수명 작성 관습
-  // handle 이벤트명
-  function handelClick() {
-    console.log("second");
+  // 브라우저는 이벤트 핸들러 메소드
+  // event 객체를 매개값으로 넣어줌
+  function handleClick(event) {
+    console.log(event.target);
+    // console.log(event.target.className);
+    // console.log(event.target.type);
   }
 
-  function handleMouseEnter() {
-    console.log("third");
-  }
-
-  function handleMouseLeave() {
-    console.log("4th");
-  }
-
+  console.log("버튼클릭1");
   return (
     <div>
-      <Button onClick={() => console.log("first")}>B1</Button>
-      <Button onClick={handelClick}>B2</Button>
-      <Button onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-        B3
+      <Button colorScheme="orange.200" onClick={handleClick}>
+        {" "}
+        button1
+      </Button>
+      <Button colorScheme="blue" onClick={handleClick}>
+        {" "}
+        button2
       </Button>
     </div>
   );
