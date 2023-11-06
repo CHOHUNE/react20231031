445ea6c3be0e35835c97128051db38d2f7bdb07c
CHOHUNE <yuureru@gmail.com>
2023-11-06 16:28:52 +0900

    immer 사용 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
-import { Box } from "@chakra-ui/react";
+import { Input, Text } from "@chakra-ui/react";
+import React from "react";
+import { useImmer } from "use-immer";
 
 function App() {
-  return <></>;
+  const [person, updatePerson] = useImmer({
+    name: "son",
+    address: { city: "seoul", country: "korea" },
+  });
+
+  function handleNameChange(e) {
+    updatePerson((draft) => {
+      draft.name = e.target.value;
+    });
+  }
+
+  function handleCityChange(e) {
+    updatePerson((draft) => {
+      draft.address.city = e.target.value;
+    });
+  }
+
+  function handleCountryChange(e) {
+    updatePerson((draft) => {
+      draft.address.country = e.target.value;
+    });
+  }
+
+  return (
+    <div>
+      <Input value={person.name} onChange={handleNameChange} />
+      <Input value={person.address.city} onChange={handleCityChange} />
+      <Input value={person.address.country} onChange={handleCountryChange} />
+      <Text>
+        {person.name}은 {person.address.country},{person.address.city}에 산다{" "}
+      </Text>
+    </div>
+  );
 }
 
 export default App;
