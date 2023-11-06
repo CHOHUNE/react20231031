25447822a1cf87c6c6d427dbe17e1998b8215f1e
CHOHUNE <yuureru@gmail.com>
2023-11-06 10:30:08 +0900

    HOOK 사용 조건 실습

--- src/App.js
+++ src/App.js
@@ -2,17 +2,10 @@ import React, { useState } from "react";
 import { Box, Button, Text } from "@chakra-ui/react";
 
 function App(props) {
-  //   HOOK use로 시작하는 함수
-  //   hook은 컴포넌트 상단에 작성해야함
-  //  조건문이나, 반복문 , 중첩된 함수에서는 사용 불가
-  function hanlde() {}
-  //    hook 호출 코드 작성불가
+  const [number, setNumber] = useState(0);
+  if (number > 5) {
+    const [text, setText] = useState("");
+  }
+  return <div></div>;
 }
-for (let i = 0; i < 3; i++) {
-  //   hook 호출 코드 작성 불가
-}
-if (true) {
-  //   hook 호출 코드 작성 불가
-}
-
 export default App;
