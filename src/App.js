d427c59197851c7ecd29cf527fedb03d94fd72e6
CHOHUNE <yuureru@gmail.com>
2023-11-07 09:51:51 +0900

    immer 사용4

--- src/App.js
+++ src/App.js
@@ -1,51 +1,76 @@
-import { Box, Button, ListItem, UnorderedList } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Box, Input, Text } from "@chakra-ui/react";
 import { useImmer } from "use-immer";
-import React from "react";
-
-let nextId = 1;
 
 function App(props) {
-  const [items, updateItems] = useImmer([]);
+  const [person1, setPerson1] = useState({
+    name: "son",
+    city: "seoul",
+    email: "son@gmail",
+  });
 
-  function handleButtonClick(todo) {
-    updateItems((draft) => {
-      draft.push({ id: nextId++, done: false, text: todo });
+  const [person2, updatePerson2] = useImmer({
+    name: "kim",
+    city: "jeju",
+    email: "kim@naver",
+  });
+
+  function handleNameChange1(e) {
+    const copyPerson1 = { ...person1 };
+
+    copyPerson1.name = e.target.value;
+    setPerson1(copyPerson1);
+  }
+
+  function handleCityChange1(e) {
+    setPerson1({ ...person1, city: e.target.value });
+  }
+
+  function handleEmailChange1(e) {
+    setPerson1({ ...person1, email: e.target.value });
+  }
+
+  function handleNameChange2(e) {
+    updatePerson2((draft) => {
+      draft.name = e.target.value;
     });
   }
 
-  function handleDoneButtonClick(id) {
-    updateItems((draft) => {
-      const itemIndex = draft.findIndex((item) => item.id === id);
-      if (itemIndex !== -1) {
-        draft[itemIndex].done = true;
-      }
+  function handleCityChange2(e) {
+    updatePerson2((draft) => {
+      draft.city = e.target.value;
+    });
+  }
+
+  function handleEmailChange2(e) {
+    updatePerson2((draft) => {
+      draft.email = e.target.value;
     });
   }
 
   return (
     <div>
-      <Button onClick={() => handleButtonClick("제육먹기")}>제육먹기</Button>
-      <Button onClick={() => handleButtonClick("수업 안듣기")}>
-        수업 안듣기
-      </Button>
-      <Button onClick={() => handleButtonClick("닭가슴살먹기")}>
-        {" "}
-        닭가슴살먹기
-      </Button>
       <Box>
-        <UnorderedList>
-          {items.map((item) => (
-            <ListItem
-              key={item.id}
-              textDecoration={item.done ? "line-through" : "none"}
-            >
-              {item.text}
-              <Button onClick={() => handleDoneButtonClick(item.id)}>
-                완료
-              </Button>
-            </ListItem>
-          ))}
-        </UnorderedList>
+        <Input value={person1.name} onChange={handleNameChange1} />
+        <Text>이름 : {person1.name}</Text>
+
+        <Input value={person1.city} onChange={handleCityChange1} />
+        <Text>도시 : {person1.city}</Text>
+
+        <Input value={person1.email} onChange={handleEmailChange1} />
+        <Text>메일 : {person1.email}</Text>
+      </Box>
+
+      <hr />
+
+      {/*Box>(Input+Text)*3*/}
+      <Box>
+        <Input value={person2.name} onChange={handleNameChange2}></Input>
+        <Text>이름 : {person2.name}</Text>
+        <Input value={person2.city} onChange={handleCityChange2}></Input>
+        <Text>도시 : {person2.city}</Text>
+        <Input value={person2.email} onChange={handleEmailChange2}></Input>
+        <Text>메일 : {person2.email}</Text>
       </Box>
     </div>
   );
