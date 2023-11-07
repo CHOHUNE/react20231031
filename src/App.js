8fca2b6bec0097531a11b47b86beb3a0de297232
CHOHUNE <yuureru@gmail.com>
2023-11-07 14:39:36 +0900

    다른 파일에 있는 Component가 Context사용하기 위해서는 Context는 export 해야함.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,6 @@
-import React, { createContext, useContext, useState } from "react";
-import { Button, Text } from "@chakra-ui/react";
-
-function AComp() {
-  // 3. context 사용하기 : useContext(Context)
-  const { setMessage } = useContext(MessageContext);
-  return (
-    <Button onClick={() => setMessage("변경된 메세지")}>메시지변경</Button>
-  );
-}
-
-function BComp() {
-  const { message } = useContext(MessageContext);
-
-  return <Text>{message}</Text>;
-}
+import React, { createContext, useState } from "react";
+import { AComp } from "./AComp";
+import { BComp } from "./BComp";
 
 function App(props) {
   const [message, setMessage] = useState("초기메세지");
@@ -35,5 +22,5 @@ function App(props) {
 
 // 1.context 만들기 : createContext(null)
 //
-const MessageContext = createContext(null);
+export const MessageContext = createContext(null);
 export default App;
