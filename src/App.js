18f7dd8118b2272e38a23ab7526a4d776b8b2d37
CHOHUNE <yuureru@gmail.com>
2023-11-08 10:24:41 +0900

    useEffect 연습 고객 번호 조회하기

--- src/App.js
+++ src/App.js
@@ -1,38 +1,52 @@
 import React, { useEffect, useState } from "react";
-import { Text, Box, Select } from "@chakra-ui/react";
+import { Box, Select, Spinner, Text } from "@chakra-ui/react";
 import axios from "axios";
 
 function App(props) {
+  const [customerIdList, setCustomerIdList] = useState([]);
   const [customerId, setCustomerId] = useState(0);
-  const [customer, setCustomer] = useState({}); // 초기 값은 빈 객체
+  const [customer, setCustomer] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
   useEffect(() => {
+    axios
+      .get("/api/main1/sub6")
+      .then((response) => setCustomerIdList(response.data));
+  }, []);
+
+  useEffect(() => {
+    setIsLoading(true);
     axios
       .get("/api/main1/sub4?id=" + customerId)
       .then((response) => response.data)
-      .then((data) => setCustomer(data)) // setCustomer가 빈 객체 초기 값으로 들어감
-      .catch((error) => console.log(error))
-      .finally(() => console.log("끝!!"));
+      .then((data) => setCustomer(data))
+      .catch((error) => setCustomer(null))
+      .finally(() => setIsLoading(false));
   }, [customerId]);
+
   return (
     <div>
       <Select
-        placeholder="고객 번호를 선택하세요 "
+        placeholder="고객 번호를 선택하세요"
         onChange={(e) => setCustomerId(e.target.value)}
       >
         {/*option[value=$]{$}*10*/}
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-        <option value="6">6</option>
-        <option value="7">7</option>
-        <option value="8">8</option>
-        <option value="9">9</option>
-        <option value="10">10</option>
+        {customerIdList.map((id) => (
+          <option value={id}>{id}</option>
+        ))}
       </Select>
       <Box>
-        <Text>고객이름 :{customer.name}</Text>
+        {isLoading && <Spinner />}
+
+        {isLoading || (
+          <>
+            {customer === null ? (
+              <Text>조회한 고객이 없습니다. 다른 번호를 선택해주세요.</Text>
+            ) : (
+              <Text>고객 이름 : {customer.name}</Text>
+            )}
+          </>
+        )}
       </Box>
     </div>
   );
